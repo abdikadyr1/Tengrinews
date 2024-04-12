9793e030afdacc60047333578705abf07cc2482e
abdikadyr1 <156767411+abdikadyr1@users.noreply.github.com>
2024-04-12 16:30:46 +0500

    minor bug fixes

--- TengriNews/parser/parse_all.py
+++ TengriNews/parser/parse_all.py
@@ -1,16 +1,12 @@
 from fastapi import Request
 from parser.parse_travel import get_content as get_travel_content, get_html as get_travel_html, URL as URL_TRAVEL
 from parser.parse_edu import get_content as get_edu_content, get_html as get_edu_html, URL as URL_EDU
-from parser.parse_relevant import get_content as get_relevant_content, get_html as get_relevant_html, URL as URL_RELEVANT
 from parser.parse_news import get_content as get_news_content, get_html as get_news_html, URL as URL_NEWS
 from parser.parse_sport import get_content as get_sport_content, get_html as get_sport_html, URL as URL_SPORT
 
 def parse_all():
     html = get_news_html(url=URL_NEWS)
     articles = get_news_content(html.text)
-
-    html = get_relevant_html(url=URL_RELEVANT)
-    relevants = get_relevant_content(html.text)
 
     html = get_sport_html(url=URL_SPORT)
     sports = get_sport_content(html.text)
@@ -24,7 +20,6 @@ def parse_all():
     mains = [articles[0], relevants[0], sports[0], travels[0], edus[0]]
     return {
         "articles" : articles,
-        "relevants" : relevants,
         "sports" : sports, 
         "travels" : travels, 
         "mains" :mains, 
